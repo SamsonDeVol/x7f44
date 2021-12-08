8261c81a6f04792a38a0e57bc651dc0b2d55c1e5
SamsonDeVol <devols@oregonstate.edu>
2021-12-08 11:21:00 -0800

    rename constant variables

--- main.rb
+++ main.rb
@@ -2,12 +2,12 @@ require_relative 'game'
 require_relative 'box'
 require_relative 'die'
 
-NUM_T = 9
-N_DICE = 2
-N_SIDES = 6
+NUM_TILES = 9
+NUM_DICE = 2
+NUM_SIDES = 6
 
-dice = (1..N_DICE).map { |i| Die.new(N_SIDES) }
-box = Box.new(NUM_T)
+dice = (1..NUM_DICE).map { |i| Die.new(NUM_SIDES) }
+box = Box.new(NUM_TILES)
 game = Game.new(box, dice)
 
 puts Game::WELCOME_MESSAGE
